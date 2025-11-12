40acfe9628fe89bf0ab163533d59c9b1e1830e7c
Yoni Wolovitz <yoni@queado.co.za>
2025-11-12 14:28:52 +0200

    Fix Vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 const base =
-  process.env.NODE_ENV === "production" ? "/Shu-Assess/" : "/";
+  process.env.NODE_ENV === "production" ? "/Shua-Assess/" : "/";
 
 export default defineConfig({
   base,
